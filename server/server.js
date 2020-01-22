11ad62bd2e4f99c07e71d024e1b3c7f221ed202f
Stephen Ramsay <rabrandom@gmail.com>
2020-01-22 11:31:00 +0000

    added router and server.js

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const bodyParser = require('bodyParser');
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('guests');
+  const bookingsCollection = db.collection('bookings');
+  const bookingsRouter = createRouter(bookingsCollection);
+  app.use('api/bookings', bookingsRouter);
+});
+.catch(console.err);
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
